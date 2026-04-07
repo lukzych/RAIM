12dd81ae4e4178756909247e23a1637ef0217900
Luki <lzych303@gmail.com>
2026-04-07 21:14:07 +0200

    dodanie wykresów

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+import csv
 
 
 app = Flask(__name__)
@@ -6,6 +7,32 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+
+@app.route("/api/ecg")
+def get_ecg():
+    data = []
+    with open('data/ECG.csv') as f:
+        reader = csv.DictReader(f)
+        for i, row in enumerate(reader):
+            if i >= 700: 
+                break
+            data.append(float(row['ECG']))
+
+    return jsonify(data)
+
+@app.route("/api/bvp")
+def get_bvp():
+    data = []
+    with open('data/BVP.csv') as f:
+        reader = csv.DictReader(f)
+        for i, row in enumerate(reader):
+            if i >= 64: 
+                break
+            data.append(float(row['BVP']))
+
+    return jsonify(data)
 
 if __name__ == "__main__":
     app.run(debug=True)
