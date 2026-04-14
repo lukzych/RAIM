e671d43f16a1d28982b4ffd467a5332ddf0f4b19
Luki <lzych303@gmail.com>
2026-04-14 16:31:46 +0200

    Dodanie bazy SQLAlchemy

--- app.py
+++ app.py
@@ -1,8 +1,24 @@
 from flask import Flask, render_template, jsonify
 import csv
 
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sensors.db'
+
+
+db = SQLAlchemy(app)
+
+class SensorData(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    sensor = db.Column(db.String(10), nullable=False)
+    value = db.Column(db.Float, nullable=False)
+
+with app.app_context():
+    db.create_all()
+
+
 
 @app.route("/")
 def index():
@@ -33,6 +49,7 @@ def get_bvp():
             data.append(float(row['BVP']))
 
     return jsonify(data)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
