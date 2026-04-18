e752e9cf255d9b45e219afacb657175ce8922ca0
Luki <lzych303@gmail.com>
2026-04-18 21:16:56 +0200

    Przygotowania do ETAPU 2 do dokonczenia

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ import csv
 
 from flask_sqlalchemy import SQLAlchemy
 
+import threading
+
+import random
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sensors.db'
@@ -14,6 +17,8 @@ class SensorData(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     sensor = db.Column(db.String(10), nullable=False)
     value = db.Column(db.Float, nullable=False)
+    timespamp = db.Column(db.Float, nullable = False)
+    latency_ms = db.Column(db.Float, nullable = False)
 
 with app.app_context():
     db.create_all()
@@ -26,6 +31,38 @@ def index():
 
 
 
+
+@app.route("/api/stream")
+def stream():
+
+    
+    def generator():
+        index_ecg = 0
+        index_bvp = 0
+
+        all_ecg = []
+        with open('data/ECG.csv') as f:
+            reader = csv.DictReader(f)
+            for row in reader:
+                all_ecg.append(float(row['ECG']))
+
+
+        data_ecg = []
+        while True:
+            data_ecg = all_ecg[index_ecg: index_ecg + 70]
+            index_ecg += 70
+
+
+            if index_ecg >= len(all_ecg):
+                index_ecg = 0
+
+        latency_ecg = random.randint(2,10)
+        latency_bvp = random.randint(2,10)
+        
+        db.session.add(SensorData(sensor="ECG"), value = data_ecg[1])
+
+
+'''
 @app.route("/api/ecg")
 def get_ecg():
     data = []
@@ -49,6 +86,7 @@ def get_bvp():
             data.append(float(row['BVP']))
 
     return jsonify(data)
+'''
 
 
 if __name__ == "__main__":
