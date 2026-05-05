b84921ed82095a437e9304950ca73c82aa74f3a8
Luki <lzych303@gmail.com>
2026-05-05 18:36:37 +0200

    Dodanie symulowanego opoznienia

--- app.py
+++ app.py
@@ -18,9 +18,13 @@ class SensorData(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     sensor = db.Column(db.String(10), nullable=False)
     value = db.Column(db.Float, nullable=False)
+    
+
+class StreamLog(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    sensor = db.Column(db.String(10), nullable=False)
     timestamp = db.Column(db.DateTime, nullable=False)
     latency_ms = db.Column(db.Float, nullable=False)
-
 
 '''
 Matematyka
@@ -39,6 +43,7 @@ do bazy leci
 with app.app_context():
     db.create_all()
     SensorData.query.delete()
+    StreamLog.query.delete()
     db.session.commit()
 
     with open('data/ECG.csv') as f:
@@ -48,9 +53,7 @@ with app.app_context():
                 break
             db.session.add(SensorData(
                 sensor='ECG',
-                value=float(row['ECG']),
-                timestamp=datetime.now(),
-                latency_ms=0
+                value=float(row['ECG'])
             ))
     db.session.commit()
 
@@ -61,9 +64,7 @@ with app.app_context():
                 break
             db.session.add(SensorData(
                 sensor='BVP',
-                value=float(row['BVP']),
-                timestamp=datetime.now(),
-                latency_ms=0
+                value=float(row['BVP'])
             ))
     db.session.commit()
 
@@ -74,9 +75,7 @@ with app.app_context():
                 break
             db.session.add(SensorData(
                 sensor='EDA',
-                value=float(row['EDA']),
-                timestamp=datetime.now(),
-                latency_ms=0
+                value=float(row['EDA'])
             ))
         db.session.commit()
 
@@ -88,38 +87,63 @@ q = queue.Queue()
 def pass_ecg_data():
     offset_ecg = 0
     while True:
+        random_delay = random.uniform(0.1, 0.2)
         with app.app_context():
-            q.put(SensorData.query.filter_by(sensor='ECG').offset(offset_ecg).limit(70).all())
+            records = SensorData.query.filter_by(sensor='ECG').offset(offset_ecg).limit(70).all()
+            q.put([{'sensor': r.sensor, 'value': r.value} for r in records])
+            db.session.add(StreamLog(
+                sensor='ECG',
+                timestamp=datetime.now(),
+                latency_ms= random_delay
+            ))
+            db.session.commit()
             offset_ecg += 70
             if offset_ecg >= 70000:
                 offset_ecg = 0
 
-        time.sleep(0.1)
+        time.sleep(0.1 + random_delay)
 
 
 def pass_bvp_data():
     offset_bvp = 0
 
     while True:
+        random_delay = random.uniform(0.1,0.2)
         with app.app_context():
-            q.put(SensorData.query.filter_by(sensor='BVP').offset(offset_bvp).limit(6).all())
+            records = SensorData.query.filter_by(sensor='BVP').offset(offset_bvp).limit(70).all()
+            q.put([{'sensor': r.sensor, 'value': r.value} for r in records])
+            db.session.add(StreamLog(
+                sensor='BVP',
+                timestamp=datetime.now(),
+                latency_ms= random_delay
+            ))
+            db.session.commit()
             offset_bvp += 6
 
             if offset_bvp >= 6400:
                 offset_bvp = 0
-        time.sleep(0.1)
+        time.sleep(0.1 + random_delay)
 
 def pass_eda_data():
     offset_eda = 0
 
     while True:
+        random_delay = random.uniform(0.1, 0.2)
         with app.app_context():
-            q.put(SensorData.query.filter_by(sensor='EDA').offset(offset_eda).limit(1).all())
-            offset_eda += 1 
+            records = SensorData.query.filter_by(sensor='EDA').offset(offset_eda).limit(70).all()
+            q.put([{'sensor': r.sensor, 'value': r.value} for r in records])
+            db.session.add(StreamLog(
+                sensor='EDA',
+                timestamp=datetime.now(),
+                latency_ms= random_delay
+            ))
+            db.session.commit()
+            offset_eda += 1
 
             if offset_eda >= 400:
                 offset_eda = 0
-        time.sleep(0.1)
+        time.sleep(0.1 + random_delay)
+
 
 
 ecg_thread = threading.Thread(target=pass_ecg_data).start()
@@ -137,8 +161,9 @@ def stream():
         while True:
 
             data = q.get()
-            sensor = data[0].sensor
-            values = [r.value for r in data]
+            if not data: continue
+            sensor = data[0]['sensor']
+            values = [r['value'] for r in data]
 
             yield f"data: {json.dumps({'sensor': sensor, 'values': values})}\n\n"
 
